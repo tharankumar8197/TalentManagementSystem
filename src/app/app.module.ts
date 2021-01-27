d9e562325686cd58c6b6765e99176f5846b2bc0d
tharankumar8197 <tharankumar8197@mail.com>
2021-01-27 10:41:39 +0530

    Created two Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTabsModule} from '@angular/material/tabs';
+import { CertComponent } from './cert/cert.component';
+import { CertificateComponent } from './certificate/certificate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CertComponent,
+    CertificateComponent
   ],
   imports: [
     BrowserModule,
